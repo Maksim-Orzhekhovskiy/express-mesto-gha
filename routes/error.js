a0119951721a00e752305acf497bd1b97b0fb94c
Maksim Orzhekhovskiy <smiley.dude@yandex.ru>
2023-04-23 10:56:58 +0600

    feat: errors+cards0

--- routes/error.js
+++ routes/error.js
@@ -4,6 +4,3 @@ const errorRouter = express.Router();
 
 const { errNotFound } = require('../controllers/error')
 
-errorRouter.all('/*', errNotFound);
-
-module.exports = errorRouter;
--- routes/index.js
+++ routes/index.js
@@ -4,10 +4,13 @@ const allRoutes = express.Router();
 
 const userRouter = require('./users');
 const cardRouter = require('./cards');
-const errorRouter = require('./error')
+// const errorRouter = require('./error')
 
 allRoutes.use("/users", userRouter);
 allRoutes.use("/cards", cardRouter);
-allRoutes.use("*", errorRouter)
+// allRoutes.use("*", errorRouter)
+// allRoutes = (req, res) => {
+//   res.status(404).send({ message: "Запрашиваемый ресурс не найден" });
+// });
 
 module.exports = allRoutes;
