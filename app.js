0ef43a52cb35eef70af549eebe0670be1fd49848
Maksim Orzhekhovskiy <smiley.dude@yandex.ru>
2023-04-23 10:34:58 +0600

    feat: errors+cards10

--- app.js
+++ app.js
@@ -1,37 +1,36 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
-const express = require('express');
-const mongoose = require('mongoose');
-const userRouter = require('./routes/users');
-const cardRouter = require('./routes/cards');
+const express = require("express");
+const mongoose = require("mongoose");
+const allRoutes = require("./routes/index");
+
 
 const { PORT = 8094 } = process.env;
-const DATABASE_URL = 'mongodb://localhost:27017/mestodb';
+const DATABASE_URL = "mongodb://localhost:27017/mestodb";
 
 const app = express();
 
 mongoose
-  .connect('mongodb://127.0.0.1:27017/mestodb')
+  .connect("mongodb://127.0.0.1:27017/mestodb")
   .then(() => {
     console.log(`Connected to database on ${DATABASE_URL}`);
   })
   .catch((err) => {
-    console.log('Error on database connection');
+    console.log("Error on database connection");
     console.error(err);
   });
 
 app.use(express.json());
 app.use((req, res, next) => {
   req.user = {
-    _id: '6443e584312c8ef2837c989b', // вставьте сюда _id созданного в предыдущем пункте пользователя
+    _id: "6443e584312c8ef2837c989b", // вставьте сюда _id созданного в предыдущем пункте пользователя
   };
   next();
 });
-app.use("/users", userRouter);
-app.use("/cards", cardRouter);
-app.use("*", indexRouter)
 
-app.patch('/404', (req, res) => {
-  res.status(404).json({ message: 'Ты ошибся парень /404' });
+app.use("/", allRoutes);
+
+app.patch("/404", (req, res) => {
+  res.status(404).json({ message: "Ты ошибся парень /404" });
 });
 
 app.listen(PORT, () => {
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,13 @@
 const express = require("express");
 
-const indexRouter = express.Router();
+const allRoutes = express.Router();
 
-indexRouter.use("*", (req, res) => {
+const userRouter = require('./users');
+const cardRouter = require('./cards');
+
+allRoutes.use("/users", userRouter);
+allRoutes.use("/cards", cardRouter);
+
+allRoutes.use("*", (req, res) => {
     res.status(404).send({ message: "Запрашиваемый ресурс не найден" });
   });
