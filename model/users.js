60e316d92f50e685b4465754bf16fca7dec10fe4
Maksim Orzhekhovskiy <smiley.dude@yandex.ru>
2023-05-03 11:20:58 +0600

    test commit32

--- model/users.js
+++ model/users.js
@@ -1,6 +1,7 @@
 // eslint-disable-next-line import/no-unresolved
-const mongoose = require('mongoose');
-const validator = require('validator');
+const mongoose = require("mongoose");
+const validator = require("validator");
+const bcrypt = require("bcrypt");
 
 const userSchema = new mongoose.Schema(
   {
@@ -9,22 +10,26 @@ const userSchema = new mongoose.Schema(
       minlength: 2,
       maxlength: 30,
       require: true,
-      default: 'Жак-Ив Кусто',
+      default: "Жак-Ив Кусто",
     },
     about: {
       type: String,
       minlength: 2,
       maxlength: 30,
       require: true,
-      default: 'Исследователь',
+      default: "Исследователь",
     },
     avatar: {
       type: String,
       require: true,
-      default: 'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png',
+      default:
+        "https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png",
       validate: {
-        validator: (url) => /^(http|https)?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9-._~:/?#[\]@!$&'()*+,;=]*)?$/im.test(url),
-        message: 'Неправильный формат ссылки на аватар',
+        validator: (url) =>
+          /^(http|https)?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9-._~:/?#[\]@!$&'()*+,;=]*)?$/im.test(
+            url
+          ),
+        message: "Неправильный формат ссылки на аватар",
       },
     },
     email: {
@@ -32,8 +37,8 @@ const userSchema = new mongoose.Schema(
       require: true,
       unique: true,
       validate: {
-        validator: validator.isEmail,
-        message: 'Неправильный формат почты',
+        validator: (email) => validator.isEmail(email),
+        message: "Неправильный формат почты",
       },
     },
     password: {
@@ -43,7 +48,26 @@ const userSchema = new mongoose.Schema(
   },
   {
     versionKey: false,
-  },
+    statics: {
+      findUserByCredentials: function (email, password) {
+        return this.findOne({ email })
+          .select("+password")
+          .then((user) => {
+            if (!user) {
+              return Promise.reject(new Error("Неправильные почта или пароль"));
+            }
+            return bcrypt.compare(password, user.password).then((matched) => {
+              if (!matched) {
+                return Promise.reject(
+                  new Error("Неправильные почта или пароль")
+                );
+              }
+              return user;
+            });
+          });
+      },
+    },
+  }
 );
 
-module.exports = mongoose.model('user', userSchema);
+module.exports = mongoose.model("user", userSchema);
