7fb439d110b77cefe17009db7371c0517814b8e2
Maksim Orzhekhovskiy <smiley.dude@yandex.ru>
2023-05-03 14:19:46 +0600

    test12

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,11 +1,10 @@
 const jwt = require("jsonwebtoken");
-const UnauthorizedError = require("../errors/unauthorizedError");
 const { NODE_ENV, SECRET_KEY } = process.env;
 
 module.exports = (req, res, next) => {
   const jwtToken = req.cookies.jwt;
   if (!jwtToken) {
-    return next(new AuthorizationError("Необходима авторизация"));
+    return next(res.status(401).send({ message: 'Необходима авторизация' }));
   }
   let payload;
   try {
@@ -14,7 +13,7 @@ module.exports = (req, res, next) => {
       NODE_ENV === "production" ? SECRET_KEY : "dev-secret-key"
     );
   } catch (err) {
-    return next(new UnauthorizedError("Необходима авторизация"));
+    return next(res.status(401).send({ message: 'Необходима авторизация' }));
   }
   req.user = payload;
   return next();
